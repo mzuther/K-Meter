6860119e2995d49efbe978c0eee14f2e551e3f9b
Martin Zuther <code@mzuther.de>
2014-10-03 18:26:29 +0200

    added g++ compiler flag "-std=c++11"

--- Builds/premake4.lua
+++ Builds/premake4.lua
@@ -86,12 +86,12 @@ solution "kmeter"
 	configuration { "Debug*" }
 		defines { "_DEBUG=1", "DEBUG=1", "JUCE_CHECK_MEMORY_LEAKS=1" }
 		flags { "Symbols", "ExtraWarnings" }
-		buildoptions { "-fno-inline", "-ggdb" }
+		buildoptions { "-fno-inline", "-ggdb", "-std=c++11" }
 
 	configuration { "Release*" }
 		defines { "NDEBUG=1", "JUCE_CHECK_MEMORY_LEAKS=0" }
 		flags { "OptimizeSpeed", "NoFramePointer", "ExtraWarnings" }
-		buildoptions { "-pipe", "-fvisibility=hidden" }
+		buildoptions { "-fvisibility=hidden", "-pipe", "-std=c++11" }
 
 	configuration { "Debug", "x32" }
 		targetsuffix "_debug"
