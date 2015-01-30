58ae8aea3832a20eeb703b575ad0ddc93f5a6a7b
Martin Zuther <code@mzuther.de>
2015-01-30 18:31:01 +0100

    updated copyrights

--- Source/resources/convert_resources.bat
+++ Source/resources/convert_resources.bat
@@ -6,7 +6,7 @@
 @rem  =======
 @rem  Implementation of a K-System meter according to Bob Katz' specifications
 @rem
-@rem  Copyright (c) 2010-2014 Martin Zuther (http://www.mzuther.de/)
+@rem  Copyright (c) 2010-2015 Martin Zuther (http://www.mzuther.de/)
 @rem
 @rem  This program is free software: you can redistribute it and/or modify
 @rem  it under the terms of the GNU General Public License as published by
