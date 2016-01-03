4843d0842db397d0766ba1354b8d96c497e3ff52
Martin Zuther <code@mzuther.de>
2016-01-03 15:28:43 +0100

    updated copyrights

--- Source/plugin_editor.cpp
+++ Source/plugin_editor.cpp
@@ -4,7 +4,7 @@
    =======
    Implementation of a K-System meter according to Bob Katz' specifications
 
-   Copyright (c) 2010-2015 Martin Zuther (http://www.mzuther.de/)
+   Copyright (c) 2010-2016 Martin Zuther (http://www.mzuther.de/)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -560,7 +560,7 @@ void KmeterAudioProcessorEditor::buttonClicked(Button *button)
 
         arrChapters.set(
             "Copyright",
-            "(c) 2010-2015 Martin Zuther\n");
+            "(c) 2010-2016 Martin Zuther\n");
 
         arrChapters.set(
             "Contributors",
