30ad32c859301b6bf7c4f319b64b94049c070a74
Martin Zuther <code@mzuther.de>
2018-02-26 22:36:09 +0100

    updated VST copy script

--- bin/copy_vst.bat
+++ bin/copy_vst.bat
@@ -1,44 +1,55 @@
 @echo off
+setlocal
 
-@setlocal
-@set rsync_path=E:\Documents\System\Tools\rsync\bin
-@set rsync_cmd=%rsync_path%\rsync.exe --archive
+set rsync_path=E:\Documents\System\Tools\rsync\bin
+set rsync_cmd=%rsync_path%\rsync.exe --archive
 
-@set destination=/cygdrive/d/Vst/32bit/radix/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo).dll"         "%destination%"
-@%rsync_cmd%          "K-Meter (Surround).dll"       "%destination%"
+set vst2_32=/cygdrive/c/Program Files (x86)/Steinberg/VSTPlugins/radix/
+set vst3_32=/cygdrive/c/Program Files (x86)/Common Files/VST3/radix/
+set vst2_64=/cygdrive/c/Program Files/Steinberg/VSTPlugins/radix/
+set vst3_64=/cygdrive/c/Program Files/Common Files/VST3/radix/
 
-@set destination=/cygdrive/d/Vst/Categories/Analyzer/Meter/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo).dll"         "%destination%"
-@%rsync_cmd%          "K-Meter (Surround).dll"       "%destination%"
+call :CopyVst ^
+ "%vst2_32%" ^
+ "vst/K-Meter (Stereo).dll" ^
+ "vst/K-Meter (Surround).dll" ^
+ "vst/kmeter"
 
-@set destination=/cygdrive/d/Plugins/VST2 (32 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo).dll"         "%destination%"
-@%rsync_cmd%          "K-Meter (Surround).dll"       "%destination%"
+call :CopyVst ^
+ "%vst3_32%" ^
+ "vst/K-Meter (Stereo).vst3" ^
+ "vst/K-Meter (Surround).vst3" ^
+ "vst/kmeter"
 
-@set destination=/cygdrive/d/Plugins/VST3 (32 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo).vst3"        "%destination%"
-@%rsync_cmd%          "K-Meter (Surround).vst3"      "%destination%"
+call :CopyVst ^
+ "%vst2_64%" ^
+ "vst/K-Meter (Stereo x64).dll" ^
+ "vst/K-Meter (Surround x64).dll" ^
+ "vst/kmeter"
 
-@set destination=/cygdrive/d/Plugins/VST2 (64 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo x64).dll"     "%destination%"
-@%rsync_cmd%          "K-Meter (Surround x64).dll"   "%destination%"
+call :CopyVst ^
+ "%vst3_64%" ^
+ "vst/K-Meter (Stereo x64).vst3" ^
+ "vst/K-Meter (Surround x64).vst3" ^
+ "vst/kmeter"
 
-@set destination=/cygdrive/d/Plugins/VST3 (64 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "kmeter"                       "%destination%"
-@%rsync_cmd%          "K-Meter (Stereo x64).vst3"    "%destination%"
-@%rsync_cmd%          "K-Meter (Surround x64).vst3"  "%destination%"
+pause
+exit /b %errorlevel%
 
-@echo.
-@pause
+
+:CopyVst
+set destination=%1
+set dll_1=%2
+set dll_2=%3
+set aux_dir=%4
+
+echo %destination%
+
+if not exist %dll_1% exit /b 1
+if not exist %dll_2% exit /b 2
+
+%rsync_cmd% --delete %aux_dir% %destination%
+%rsync_cmd%          %dll_1%   %destination%
+%rsync_cmd%          %dll_2%   %destination%
+
+exit /b 0
