6a6064d4477ea9035b3a7ce1d82b35405ba9527a
Martin Zuther <code@mzuther.de>
2020-05-01 17:05:47 +0200

    prettify shell scripts

--- bin/finalise_binaries.sh
+++ bin/finalise_binaries.sh
@@ -42,12 +42,12 @@ function finalise_binary
 
     if [ -f "./$filepath" ]; then
         if [ ! -f "$binary_dir/$filename" ] || [ "./$filepath" -nt "$binary_dir/$filename" ]; then
-            echo "  Finalising:  $filepath -->"
-            echo "               $binary_dir/$filename"
+            printf "  Finalising binary:   %s -->\n" "$filepath"
+            printf "                       %s\n"     "$binary_dir/$filename"
 
             cp "./$filepath" "./$binary_dir/$filename"
 
-            echo
+            printf "\n"
         fi
     fi
 }
@@ -60,64 +60,62 @@ function finalise_symbols
 
     if [ -f "./$filepath" ]; then
         if [ ! -f "$binary_dir/debug_symbols/$filepath" ] || [ "./$filepath" -nt "$binary_dir/debug_symbols/$filepath" ]; then
-            echo "  Finalising:  $filepath -->"
-            echo "               $binary_dir/debug_symbols/$filepath"
+            printf "  Finalising symbols:  %s -->\n" "$filepath"
+            printf "                       %s\n"     "$binary_dir/debug_symbols/$filepath"
 
             mkdir -p "$(dirname "./$binary_dir/debug_symbols/$filepath")"
             cp "./$filepath" "./$binary_dir/debug_symbols/$filepath"
 
-            echo
+            printf "\n"
         fi
     fi
 }
 
 
-echo
-echo "  === Finalising binaries ==="
-echo
+printf "\n  === Finalising binaries ===\n\n"
 
-finalise_binary "standalone/kmeter_stereo"
-finalise_binary "standalone/kmeter_surround"
+mkdir -p "$binary_dir/debug_symbols"
 
-finalise_binary "vst2/kmeter_stereo_vst2.so"
-finalise_binary "vst2/kmeter_surround_vst2.so"
+finalise_binary  "standalone/kmeter_stereo"
+finalise_binary  "standalone/kmeter_surround"
 
-finalise_binary "standalone/kmeter_stereo_x64"
-finalise_binary "standalone/kmeter_surround_x64"
+finalise_binary  "vst2/kmeter_stereo_vst2.so"
+finalise_binary  "vst2/kmeter_surround_vst2.so"
 
-finalise_binary "vst2/kmeter_stereo_vst2_x64.so"
-finalise_binary "vst2/kmeter_surround_vst2_x64.so"
+finalise_binary  "standalone/kmeter_stereo_x64"
+finalise_binary  "standalone/kmeter_surround_x64"
 
-finalise_binary "standalone/K-Meter (Stereo).exe"
-finalise_symbols    "standalone/K-Meter (Stereo).pdb"
-finalise_binary "standalone/K-Meter (Surround).exe"
-finalise_symbols    "standalone/K-Meter (Surround).pdb"
+finalise_binary  "vst2/kmeter_stereo_vst2_x64.so"
+finalise_binary  "vst2/kmeter_surround_vst2_x64.so"
 
-finalise_binary "vst2/K-Meter (Stereo).dll"
-finalise_symbols    "vst2/K-Meter (Stereo).pdb"
-finalise_binary "vst2/K-Meter (Surround).dll"
-finalise_symbols    "vst2/K-Meter (Surround).pdb"
+finalise_binary  "standalone/K-Meter (Stereo).exe"
+finalise_symbols "standalone/K-Meter (Stereo).pdb"
+finalise_binary  "standalone/K-Meter (Surround).exe"
+finalise_symbols "standalone/K-Meter (Surround).pdb"
 
-finalise_binary "vst3/K-Meter (Stereo).vst3"
-finalise_symbols    "vst3/K-Meter (Stereo).pdb"
-finalise_binary "vst3/K-Meter (Surround).vst3"
-finalise_symbols    "vst3/K-Meter (Surround).pdb"
+finalise_binary  "vst2/K-Meter (Stereo).dll"
+finalise_symbols "vst2/K-Meter (Stereo).pdb"
+finalise_binary  "vst2/K-Meter (Surround).dll"
+finalise_symbols "vst2/K-Meter (Surround).pdb"
 
-finalise_binary "standalone/K-Meter (Stereo x64).exe"
-finalise_symbols    "standalone/K-Meter (Stereo x64).pdb"
-finalise_binary "standalone/K-Meter (Surround x64).exe"
-finalise_symbols    "standalone/K-Meter (Surround x64).pdb"
+finalise_binary  "vst3/K-Meter (Stereo).vst3"
+finalise_symbols "vst3/K-Meter (Stereo).pdb"
+finalise_binary  "vst3/K-Meter (Surround).vst3"
+finalise_symbols "vst3/K-Meter (Surround).pdb"
 
-finalise_binary "vst2/K-Meter (Stereo x64).dll"
-finalise_symbols    "vst2/K-Meter (Stereo x64).pdb"
-finalise_binary "vst2/K-Meter (Surround x64).dll"
-finalise_symbols    "vst2/K-Meter (Surround x64).pdb"
+finalise_binary  "standalone/K-Meter (Stereo x64).exe"
+finalise_symbols "standalone/K-Meter (Stereo x64).pdb"
+finalise_binary  "standalone/K-Meter (Surround x64).exe"
+finalise_symbols "standalone/K-Meter (Surround x64).pdb"
 
-finalise_binary "vst3/K-Meter (Stereo x64).vst3"
-finalise_symbols    "vst3/K-Meter (Stereo x64).pdb"
-finalise_binary "vst3/K-Meter (Surround x64).vst3"
-finalise_symbols    "vst3/K-Meter (Surround x64).pdb"
+finalise_binary  "vst2/K-Meter (Stereo x64).dll"
+finalise_symbols "vst2/K-Meter (Stereo x64).pdb"
+finalise_binary  "vst2/K-Meter (Surround x64).dll"
+finalise_symbols "vst2/K-Meter (Surround x64).pdb"
 
-echo "  Done."
-echo
-echo
+finalise_binary  "vst3/K-Meter (Stereo x64).vst3"
+finalise_symbols "vst3/K-Meter (Stereo x64).pdb"
+finalise_binary  "vst3/K-Meter (Surround x64).vst3"
+finalise_symbols "vst3/K-Meter (Surround x64).pdb"
+
+printf "  Done.\n\n\n"
