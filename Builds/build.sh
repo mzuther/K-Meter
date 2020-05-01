0eba6dec3a608d0d1d8f0407d06d348e7ab1282c
Martin Zuther <code@mzuther.de>
2020-05-01 11:19:33 +0200

    simplify build script

--- Builds/build.sh
+++ Builds/build.sh
@@ -57,17 +57,17 @@ case $1 in
 	u)
 		MAKEFILE="unittest"
 		MAKEFILE_PRINT="Unit tests"
-		EXECUTABLE="unittest"
+		EXECUTABLE="unittest/unittest"
 		;;
 	1)
 		MAKEFILE="kmeter_standalone_stereo"
 		MAKEFILE_PRINT="Standalone (Stereo)"
-		EXECUTABLE="kmeter_stereo"
+		EXECUTABLE="standalone/kmeter_stereo"
 		;;
 	2)
 		MAKEFILE="kmeter_standalone_surround"
 		MAKEFILE_PRINT="Standalone (Surround)"
-		EXECUTABLE="kmeter_surround"
+		EXECUTABLE="standalone/kmeter_surround"
 		;;
 	3)
 		MAKEFILE="kmeter_vst2_stereo"
@@ -121,21 +121,11 @@ make --directory=linux/gmake/ --no-print-directory config=$PLATFORM $MAKEFILE $*
 
 # compilation was successful
 if [ $? -eq 0 ]; then
-	echo $MAKEFILE | grep "_standalone_" > /dev/null
-
-	# target is standalone, so execute file
-	if [ $? -eq 0 ]; then
-        echo $EXECUTABLE$EXECUTABLE_EXTENSION
+	# execute file if possible
+    if [ -n "$EXECUTABLE" ]; then
 		echo
-		../bin/standalone/$EXECUTABLE$EXECUTABLE_EXTENSION
-	fi
-
-	echo $MAKEFILE | grep "unittest" > /dev/null
-
-	# target is unit test, so execute file
-	if [ $? -eq 0 ]; then
-        echo $EXECUTABLE$EXECUTABLE_EXTENSION
+        echo "Executing \"$EXECUTABLE$EXECUTABLE_EXTENSION\"..."
 		echo
-		../bin/unittest/$EXECUTABLE$EXECUTABLE_EXTENSION
+		../bin/$EXECUTABLE$EXECUTABLE_EXTENSION
 	fi
 fi
