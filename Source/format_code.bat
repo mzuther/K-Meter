ca755742e3b4c926a9b3976eff6c5679971992b6
Martin Zuther <code@mzuther.de>
2020-05-02 15:08:38 +0200

    automate creation of formatting scripts

--- Source/format_code.bat
+++ Source/format_code.bat
@@ -1,29 +1,36 @@
 @echo off
 
-rem ----------------------------------------------------------------------------
-rem
+rem  ----------------------------------------------------------------------------
+rem  
 rem  K-Meter
 rem  =======
 rem  Implementation of a K-System meter according to Bob Katz' specifications
-rem
+rem  
 rem  Copyright (c) 2010-2020 Martin Zuther (http://www.mzuther.de/)
-rem
+rem  
 rem  This program is free software: you can redistribute it and/or modify
 rem  it under the terms of the GNU General Public License as published by
 rem  the Free Software Foundation, either version 3 of the License, or
 rem  (at your option) any later version.
-rem
+rem  
 rem  This program is distributed in the hope that it will be useful,
 rem  but WITHOUT ANY WARRANTY; without even the implied warranty of
 rem  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 rem  GNU General Public License for more details.
-rem
+rem  
 rem  You should have received a copy of the GNU General Public License
 rem  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-rem
+rem  
 rem  Thank you for using free software!
+rem  
+rem  ----------------------------------------------------------------------------
+
+
+rem  ############################################################################
 rem
-rem ----------------------------------------------------------------------------
+rem  WARNING: this file is auto-generated, please do not edit!
+rem
+rem  ############################################################################
 
 @AStyle --recursive --exclude="resources" --options=./.astylerc "*.cpp"
 @echo.
