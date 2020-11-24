375df564712642412a117463e6f50fd34c5a1c2b
Martin Zuther <code@mzuther.de>
2020-11-24 13:59:16 +0100

    change structure of skin directory

---
+++ skins/Source/skin.h
@@ -0,0 +1,345 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace skin
+{
+    extern const char*  Skin_xml;
+    const int           Skin_xmlSize = 39514;
+
+  #ifdef BACKGROUND
+    extern const char*  itu_stereo_svg;
+    const int           itu_stereo_svgSize = 33250;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k14_over_svg;
+    const int           reference_level_k14_over_svgSize = 9792;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  dim_on_svg;
+    const int           dim_on_svgSize = 9954;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k20_off_svg;
+    const int           reference_level_k20_off_svgSize = 14373;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  skin_over_svg;
+    const int           skin_over_svgSize = 10554;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mono_on_svg;
+    const int           mono_on_svgSize = 10553;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_normal_off_svg;
+    const int           reference_level_normal_off_svgSize = 17532;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  expand_on_svg;
+    const int           expand_on_svgSize = 11927;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k12_off_svg;
+    const int           reference_level_k12_off_svgSize = 14097;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k14_off_svg;
+    const int           reference_level_k14_off_svgSize = 13905;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  skin_off_svg;
+    const int           skin_off_svgSize = 14666;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  about_over_svg;
+    const int           about_over_svgSize = 11197;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  about_off_svg;
+    const int           about_off_svgSize = 16111;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_normal_on_svg;
+    const int           reference_level_normal_on_svgSize = 11792;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  discrete_off_svg;
+    const int           discrete_off_svgSize = 20882;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_normal_over_svg;
+    const int           reference_level_normal_over_svgSize = 11794;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  hold_over_svg;
+    const int           hold_over_svgSize = 10302;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  hold_on_svg;
+    const int           hold_on_svgSize = 10300;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  expand_over_svg;
+    const int           expand_over_svgSize = 11929;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  discrete_on_svg;
+    const int           discrete_on_svgSize = 13520;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mono_over_svg;
+    const int           mono_over_svgSize = 10554;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k14_on_svg;
+    const int           reference_level_k14_on_svgSize = 9790;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mono_off_svg;
+    const int           mono_off_svgSize = 14668;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  skin_on_svg;
+    const int           skin_on_svgSize = 10552;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k12_on_svg;
+    const int           reference_level_k12_on_svgSize = 9980;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  discrete_over_svg;
+    const int           discrete_over_svgSize = 13522;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_rms_on_svg;
+    const int           mode_rms_on_svgSize = 10089;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  flip_off_svg;
+    const int           flip_off_svgSize = 14220;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_rms_over_svg;
+    const int           mode_rms_over_svgSize = 10091;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  flip_over_svg;
+    const int           flip_over_svgSize = 10120;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reset_on_svg;
+    const int           reset_on_svgSize = 11792;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k20_on_svg;
+    const int           reference_level_k20_on_svgSize = 10263;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  validate_off_svg;
+    const int           validate_off_svgSize = 20616;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k20_over_svg;
+    const int           reference_level_k20_over_svgSize = 10265;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reset_over_svg;
+    const int           reset_over_svgSize = 11795;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mute_on_svg;
+    const int           mute_on_svgSize = 10576;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mute_over_svg;
+    const int           mute_over_svgSize = 10578;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  about_on_svg;
+    const int           about_on_svgSize = 11195;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_rms_off_svg;
+    const int           mode_rms_off_svgSize = 13394;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  hold_off_svg;
+    const int           hold_off_svgSize = 14412;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  validate_over_svg;
+    const int           validate_over_svgSize = 13284;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reset_off_svg;
+    const int           reset_off_svgSize = 16715;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  flip_on_svg;
+    const int           flip_on_svgSize = 10119;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  dim_over_svg;
+    const int           dim_over_svgSize = 9957;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  peaks_on_svg;
+    const int           peaks_on_svgSize = 11642;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  peaks_off_svg;
+    const int           peaks_off_svgSize = 16570;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  expand_off_svg;
+    const int           expand_off_svgSize = 17670;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_itu_off_svg;
+    const int           mode_itu_off_svgSize = 15266;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  peaks_over_svg;
+    const int           peaks_over_svgSize = 11644;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_itu_on_svg;
+    const int           mode_itu_on_svgSize = 10350;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mode_itu_over_svg;
+    const int           mode_itu_over_svgSize = 10352;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  validate_on_svg;
+    const int           validate_on_svgSize = 13282;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  mute_off_svg;
+    const int           mute_off_svgSize = 14694;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  reference_level_k12_over_svg;
+    const int           reference_level_k12_over_svgSize = 9982;
+
+  #endif
+  #ifdef BUTTON
+    extern const char*  dim_off_svg;
+    const int           dim_off_svgSize = 13262;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  peak_off_svg;
+    const int           peak_off_svgSize = 7069;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  debug_svg;
+    const int           debug_svgSize = 10138;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  true_peak_off_svg;
+    const int           true_peak_off_svgSize = 7230;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  true_peak_on_svg;
+    const int           true_peak_on_svgSize = 7229;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  overflow_off_svg;
+    const int           overflow_off_svgSize = 7080;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  overflow_on_svg;
+    const int           overflow_on_svgSize = 7079;
+
+  #endif
+  #ifdef LABEL
+    extern const char*  peak_on_svg;
+    const int           peak_on_svgSize = 7063;
+
+  #endif
+  #ifdef METER
+    extern const char*  phase_correlation_needle_svg;
+    const int           phase_correlation_needle_svgSize = 7377;
+
+  #endif
+  #ifdef METER
+    extern const char*  itu_stereo_svg;
+    const int           itu_stereo_svgSize = 6955;
+
+  #endif
+  #ifdef METER
+    extern const char*  stereo_needle_svg;
+    const int           stereo_needle_svgSize = 7333;
+
+  #endif
+  #ifdef METER
+    extern const char*  stereo_svg;
+    const int           stereo_svgSize = 9738;
+
+  #endif
+  #ifdef METER
+    extern const char*  phase_correlation_svg;
+    const int           phase_correlation_svgSize = 10133;
+
+  #endif
+}
